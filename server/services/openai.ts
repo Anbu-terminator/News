364fc2d9cd6c26af6c8965f85282eca4c5c2f71b
Anbu Sivam.B <anbulegend101@gmail.com>
2025-09-30 01:19:00 +0530

    openai.ts

--- server/services/openai.ts
+++ server/services/openai.ts
@@ -19,7 +19,7 @@ interface TranscriptResponse {
 
 // -------------------- CONFIG --------------------
 const HUGGINGFACE_API_KEY =
-  process.env.HF_TOKEN || "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";
+  process.env.HUGGINGFACE_API_KEY || process.env.HF_TOKEN;
 const YOUTUBE_API_KEY = "AIzaSyDx0Ayoy52jC6f6ZvNyW1B_biL7Bjjgh34";
 const SUMMARIZER_MODEL = "facebook/bart-large-cnn";
 
